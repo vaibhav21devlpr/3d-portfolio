c278cdceef032673c04abd384e26c0d74bcb2771
Vaibhav Pandey <vaibhavpandey791@gmail.comclear>
2025-09-28 11:25:01 +0530

    [Fixed errors]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
-import NavBar from "./components/Navbar"
+import NavBar from "./components/NavBar"
 import LogoSection from "./sections/LogoSection"
 import FeatureCards from "./sections/FeatureCards"
 import ExperienceSection from "./sections/ExperienceSection"
